03cb622365a80ff77ddd949271332e6eafc8d2dd
Thinh An <thinhan1701@gmail.com>
2025-06-19 16:27:09 +0700

    Add cors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -28,6 +28,16 @@ func (app *application) routes() http.Handler {
 		MaxAge:           300, // Maximum value not ignored by any major browsers
 	}))
 	
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	  }))
 	mux.Route("/posts", func(r chi.Router) {
 		r.With(app.authMiddleware).With(app.paginate).Get("/", app.getPostsHandler)
 		r.With(app.authMiddleware).Post("/", app.createPostHandler)
